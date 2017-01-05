4bb5cb55691f0d708deec1c965558e9523c414e9
Akijunior <suitsu19@gmail.com>
2017-01-05 14:13:45 -0300

    Create EncontrarCharVersaoDois.c

---
+++ EncontrarCharVersaoDois.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int caracter(char c, char s[]) {
+
+    if (s[0] == '\0')
+        return 0;
+    if (s[0] == c)
+        return (1 + caracter(c, ++s));
+    return caracter(c, ++s);
+}
+
+main() {
+
+    char s[30], c;
+    int t;
+    printf("Busca string\n\n");
+    printf("\nDigite a string: ");
+    gets(s);
+    //scanf("%s", s);
+
+    fflush(stdin);
+    printf("\nDigite o caractere desejado: ");
+    //scanf("%c", &c);
+    c = getchar();
+    t = caracter(c, s);
+    printf("\n\nEncontrei %d vezes.", t);
+    //printf("%d", t);
+
+}
